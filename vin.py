15470192dc7bc9428714d3a4685b0262acc3ff7a
nred <oilsafats@gmail.com>
2022-02-27 14:20:34 -0500

    Added : goto and fixed line length crashes

--- vin.py
+++ vin.py
@@ -2,15 +2,57 @@ import curses
 import sys
 import os
 
+y = 0
+posy = 0
+x = 0
+posx = 0
+lines = []
+
+def checkX(cols):
+    global y, posy, x, posx, lines
+    if posx + x > len(lines[posy + y]) - 1:
+        if len(lines[posy + y]) < cols:
+            posx = 0
+        else:
+            posx = x - cols + 1
+        x = len(lines[posy + y]) - posx - 1
+
+def checkY(rows):
+    global y, posy, x, posx, lines
+    if y < posy:
+        posy = 0
+    if posy + y > rows - 2:
+        if len(lines) < rows:
+            posy = 0
+        else:
+            posy = y + 2 - rows
+        y = y - posy
+
+def padStr(size, *strings):
+    strings = [str(string) for string in strings]
+    endStr = ' '.join(strings[1:])
+    string = strings[0] + ' ' * (size - len(strings[0] + endStr)) + endStr
+    return string
+
+def modeStr(mode):
+    if mode == 'None':
+        return ''
+    elif ':' in mode:
+        return mode
+    else:
+        return ' --' + mode + '--'
+
 def main(stdscr, file):
+    global y, posy, x, posx, lines
+    key = ''
+    mode = 'None'
+    quit = False
     curses.set_escdelay(50)
     stdscr.clear()
     
     curses.curs_set(1)
     rows, cols = stdscr.getmaxyx()
     
-    
-    lines = []
     if os.path.exists(file):
         with open(file, 'r') as f:
             for line in f:
@@ -18,24 +60,18 @@ def main(stdscr, file):
     else:
         lines.append('')
     
-    y = 0
-    posy = 0
-    x = 0
-    posx = 0
-    key = ''
-    mode = 'None'
-    quit = False
     while not quit:
+        if x < 0: x = 0
+        elif x >= cols - 1: x = cols - 1
+        if y < 0: y = 0
+        elif y >= rows - 1: y = rows - 2
+        
         for i in range(rows - 1):
             if i >= len(lines):
                 stdscr.addstr(i, 0, '~' + ' ' * (cols - 2))
             else:
-                stdscr.addstr(i, 0, lines[i + posy][posx:] + ' ' * (cols - 1 - len(lines[i + posy])))
-        stdscr.addstr(rows - 1, 0, mode + ' ' * (cols - 1 - len(mode)))
-        if x < 0: x = 0
-        elif x >= cols - 1: x = cols - 1
-        if y < 0:y = 0
-        elif y >= rows - 1: y = rows - 2
+                stdscr.addstr(i, 0,  padStr(cols - 1, lines[i + posy][posx:posx + cols - 1]))
+        stdscr.addstr(rows - 1, 0, padStr(cols - 1, modeStr(mode), x + posx, y + posy))
         stdscr.move(y, x)
         stdscr.refresh()
 
@@ -52,12 +88,13 @@ def main(stdscr, file):
                     posy += 1
                 elif y < rows - 2:
                     y += 1
-               
+                checkX(cols)
         elif key == curses.KEY_UP:
             if posy > 0 and y < rows // 3:
                 posy -= 1
             elif y > 0:
                 y -= 1
+            checkX(cols)
         elif key == curses.KEY_RIGHT:
             if x < cols and x < len(lines[posy + y]) - 1:
                 if posx + cols < len(lines[posy + y]) and posx + x > cols // 3 * 2:
@@ -89,7 +126,14 @@ def main(stdscr, file):
             elif key == ord('i'):
                 mode = 'insert'
         elif mode.startswith(':'):
-            if key == ord('\n'):
+            if key == curses.KEY_BACKSPACE:
+                mode = mode[0: len(mode) - 1]
+                if mode == '': mode = 'None'
+            elif key == ord('\n'):
+                if mode[1:].isnumeric():
+                    y = int(mode[1:])
+                    checkY(rows)
+                    x = 0
                 if 'w' in mode:
                     with open(file, 'w') as w:
                         for line in lines:
@@ -100,7 +144,8 @@ def main(stdscr, file):
                 if 'q' in mode:
                     quit = True
                 mode = 'None'
-            mode += chr(key)
+            else:
+                mode += chr(key)
         elif mode == 'insert':
             if key == curses.KEY_BACKSPACE:
                 if x > 0:
